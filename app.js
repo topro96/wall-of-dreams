2fe3116b12904e3d17520b7e91ecc0a30ea9ebbb
Topro <buiminhthanh1396@gmail.com>
2020-05-17 15:17:40 +0700

    mongodb

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+require('dotenv').config();
 
 app.get('/', (req, res) => {
     res.send("Hello World");
 })
 
 const port = process.env.port || 5000;
+
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
+
+const connection = mongoose.connection;
+
+connection.once('open', () => {
+    console.log("MongoDB database connection established successfully");
+});
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
