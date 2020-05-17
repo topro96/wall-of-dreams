e8e392b00bf67bb33f9d43d8f8420b750afaba07
Topro <buiminhthanh1396@gmail.com>
2020-05-17 14:51:43 +0700

    change port

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ app.get('/', (req, res) => {
     res.send("Hello World");
 })
 
-const port = process.env.PORT || 5000;
+const port = process.env.port || 5000;
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
